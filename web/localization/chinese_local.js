2168605a893cc8be56adf076ce968a781bba3114
mohan.tadikimalla <mohan.tadikimalla@fireeye.com>
2014-09-03 18:59:04 +0530

    Localization task

---
+++ web/localization/chinese_local.js
@@ -0,0 +1,14 @@
+/**
+ * New node file
+ */
+
+var settingsProvider = angular.module("LoginFormTexts", []);
+
+settingsProvider.value("providedValue", {
+    UserName: "中國哲學書電子化計劃",
+    Password: "中國哲學書電子化計劃",
+    Login: "中國哲學書電子化計劃"
+});
+
+
+
---
+++ web/localization/english_local.js
@@ -0,0 +1,11 @@
+/**
+ * New node file
+ */
+
+var settingsProvider = angular.module("LoginFormTexts", []);
+
+settingsProvider.value("providedValue", {
+    Email: "Email",
+    Password: "Password",
+    Login: "Login"
+});
